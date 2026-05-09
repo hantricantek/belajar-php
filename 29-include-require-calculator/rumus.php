6dc84d68d7e9b6c11cf0e4e8e2b0e799081293b2
hantri48-eng <hantrihantri48@gmail.com>
2026-05-09 15:52:08 +0700

    menambahkan

--- 29-include-require-calculator/rumus.php
+++ 29-include-require-calculator/rumus.php
@@ -2,20 +2,20 @@
 
 // 1. menghitung luas persegi panjang
 // 2. menghitung luas persegi
-// 3. menghitung luas segi tiga
+// 3. menghitung luas segitiga
 
 function luasPersegiPanjang(int $panjang, int $lebar){
     $luas = $panjang * $lebar;
     return $luas;
 }
 
-function luasPersegi (int $sisi){
+function luasPersegi(int $sisi){
     $luas = $sisi * $sisi;
     return $luas;
 }
 
-function luasSegitiga (int $alas, int $tinggi){
+// tugas rumus segi tiga
+function luasSegitiga(int $alas, int $tinggi){
     $luas = 0.5 * $alas * $tinggi;
     return $luas;
 }
-
--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include "rumus.php";
-
-$luasPersegiPanjang = luasPersegiPanjang(4,5);
-echo "luas persegi panjang adalah: " . $luasPP . PHP_EOL;
-
-$luasPP = luasPersegi(5);
-echo "Luas persegi panjang".$luasPP;
